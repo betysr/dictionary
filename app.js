3e6448d1c8fad4fe0704729dffa604ce0513808a
Betul <111394093+betysr@users.noreply.github.com>
2022-09-19 13:23:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,3 @@ btn.addEventListener("click", () => {
             result.innerHTML = `<h3 class="error">Couldn't Find The Word</h3>`;
         })
 });
-
-function playSound() {
-    sound.play();
-}
